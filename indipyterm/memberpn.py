fc9704642b58f15e4e144b3f045dfda16c8a0cd3
Bernard Czenkusz <bernie@skipole.co.uk>
2024-12-17 17:24:23 +0000

    Working on members

--- indipyterm/memberpn.py
+++ indipyterm/memberpn.py
@@ -1,7 +1,7 @@
 from typing import Iterable
 
 from textual.app import App, ComposeResult, SystemCommand
-from textual.widgets import Footer, Static, Button, Log, Input, TabbedContent, TabPane, Rule
+from textual.widgets import Footer, Static, Button, Log, Input, TabbedContent, TabPane, Switch
 from textual.reactive import reactive
 from textual.screen import Screen
 from textual.containers import Container, Horizontal, VerticalScroll, Center
@@ -11,22 +11,64 @@ from .connections import get_connection, get_devicename, get_devicemessages, get
 from textual.widget import Widget
 
 
+class MemberLabel(Static):
+
+    DEFAULT_CSS = """
+        MemberLabel {
+            margin-right: 4;
+            width: auto;
+        }
+        """
+
+
 class SwitchValue(Static):
+
+    DEFAULT_CSS = """
+        SwitchValue {
+            margin-right: 4;
+            width: auto;
+            padding: 1;
+        }
+        """
 
     mvalue = reactive("")
 
+    def __init__(self, switchval):
+        super().__init__(switchval)
+        if switchval == "On":
+            self.styles.background = "darkgreen"
+            self.styles.color = "white"
+        elif switchval == "Off":
+            self.styles.background = "red"
+            self.styles.color = "white"
+        else:
+            self.styles.background = "black"
+            self.styles.color = "white"
+
     def watch_mvalue(self, mvalue):
         if mvalue:
+            if mvalue == "On":
+                self.styles.background = "darkgreen"
+                self.styles.color = "white"
+            elif mvalue == "Off":
+                self.styles.background = "red"
+                self.styles.color = "white"
+            else:
+                mvalue = "?"
+                self.styles.background = "black"
+                self.styles.color = "white"
             self.update(mvalue)
+
 
 
 class SwitchMemberPane(Widget):
 
     DEFAULT_CSS = """
         SwitchMemberPane {
-            layout: vertical;
+            layout: horizontal;
             background: $panel;
             margin-left: 1;
+            margin-bottom: 1;
             height: auto;
         }
         """
@@ -41,11 +83,13 @@ class SwitchMemberPane(Widget):
 
     def compose(self):
         "Draw the member"
-        yield Static(self.member.label)
+        yield MemberLabel(self.member.label)
         yield SwitchValue(self.member.membervalue).data_bind(SwitchMemberPane.mvalue)
-
-
-
+        if self.vector.perm != "ro":
+            if self.member.membervalue == "On":
+                yield Switch(value=True)
+            else:
+                yield Switch(value=False)
 
 
 
